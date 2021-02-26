00f65a924a6d7aec3fdab785d92a7d29cc87b04d
Айдар <bajanga07@gmail.com>
2021-02-26 20:47:56 +0700

    Validation and saves to Local storage
    Updated validation to username and password (no validation for repeated emails and logins). Added submition to Local storage. Chaned UI for validations.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './App.scss';
+import './tooltip.scss';
+
+function validateInput(value, regex, target) {
+    if (value.match(regex) === null) {
+        target.classList.toggle("error__pass");
+        setTimeout(() => target.classList.toggle("error__pass"), 820);
+        return false;
+    }
+    return true;
+}
 
 export class Form extends React.Component {
     constructor(props) {
@@ -7,83 +17,190 @@ export class Form extends React.Component {
         this.state = {
             valueEmail: '',
             valuePass: '',
-            length: 0,
-            passShow: false,
-            checkNumber: false,
-            checkUpper: false,
-            checkLower: false
+            valueUser: '',
+            passwordCheck: {
+                length: 0,
+                checkNumber: false,
+                checkUpper: false,
+                checkLower: false
+            },
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.togglePassword = this.togglePassword.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleEmailUserChange = this.handleEmailUserChange.bind(this);
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        let inputPass = event.target[1];
-        let regex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[A-Za-z0-9 !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]{8,}&/;
-        if (!inputPass.value.match(regex)) {
-            inputPass.classList.toggle("error__pass");
-            setTimeout(() => inputPass.classList.toggle("error__pass"), 820);
+        let inputUser = event.target[0],
+            inputEmail = event.target[1],
+            inputPass = event.target[2],
+            inputCheck = true,
+            userValidRegex = /^[a-zA-z0-9._-]{4,32}$/,
+            emailValidRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            passValidRegex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[A-Za-z0-9 !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]{8,}$/;
+        inputCheck = inputCheck && validateInput(this.state.valueUser, userValidRegex, inputUser);
+        inputCheck = inputCheck && validateInput(this.state.valueEmail, emailValidRegex, inputEmail);
+        inputCheck = inputCheck && validateInput(this.state.valuePass, passValidRegex, inputPass);
+        if (inputCheck) {
+            localStorage.setItem(inputUser.value, JSON.stringify({
+                user: inputUser.value, 
+                email: inputEmail.value, 
+                password: inputPass.value
+            }));
         }
     }
 
-    handleChange(e) {
-        this.setState({ [e.target.type === "email" ? "valueEmail" : "valuePass"]: e.target.value });
-        if (e.target.type === "password") {
-            let regexLower = /(?=.*[a-z])/;
-            let regexUpper = /(?=.*[A-Z])/;
-            let regexNumber = /(?=.*[0-9])/;
-            this.setState({
+    handleEmailUserChange(e) {
+        this.setState({ [e.target.type === "email" ? "valueEmail" : "valueUser"]: e.target.value });
+    }
+
+    handlePasswordChange(e) {
+        this.setState({ "valuePass": e.target.value });
+        let regexLower = /(?=.*[a-z])/,
+            regexUpper = /(?=.*[A-Z])/,
+            regexNumber = /(?=.*[0-9])/;
+        this.setState({
+            passwordCheck: {
                 length: e.target.value.length,
                 checkLower: e.target.value.match(regexLower) ? true : false,
                 checkUpper: e.target.value.match(regexUpper) ? true : false,
                 checkNumber: e.target.value.match(regexNumber) ? true : false
-            });
-        }
-    }
-
-    togglePassword(event) {
-        this.setState({passShow: this.state.passShow ? false : true});
-        event.target.classList.toggle("fa-eye-slash");
+            }
+        });
     }
 
     render() {
-        return (<div>
+        return (<div className="wrapper">
             <form onSubmit={this.handleSubmit}>
-                <input
-                    placeholder="Email"
-                    onChange={this.handleChange}
-                    type="email"
-                    value={this.state.valueEmail}
-                ></input><br />
-                <label>
-                <input
-                    placeholder="Password"
-                    onChange={this.handleChange}
-                    type={this.state.passShow ? "text" : "password"}
+                <p>Registration with Email</p>
+                <Username userValuePass={this.handleEmailUserChange} value={this.state.valueUser} />
+                <Email emailValuePass={this.handleEmailUserChange} value={this.state.valueEmail} />
+                <Password
+                    passwordValuePass={this.handlePasswordChange}
                     value={this.state.valuePass}
-                ></input><i className="far fa-eye" onClick={this.togglePassword}></i></label><br />
-                <span 
-                    className={this.state.length > 7  ? "correct" : "incorrect"}
-                >* Password must be longer than 7 characters</span><br/>
-                <span 
-                    className={this.state.checkNumber ? "correct" : "incorrect"}
-                >* Password must contain at least one number</span><br/>
-                <span 
-                    className={this.state.checkLower && this.state.checkUpper ? "correct" : "incorrect"}
-                >* Password must contain at least one upper and lower case letters</span><br/>
-                <input type="submit" />
+                    passCheck={this.state.passwordCheck}
+                />
+                <input type="submit" value="Submit"></input>
             </form>
         </div>);
     }
 }
 
+class Username extends React.Component {
+    constructor(props) {
+        super(props);
+        this.userHandle = this.userHandle.bind(this);
+    }
+
+    userHandle(e) {
+        this.props.userValuePass(e);
+    }
+
+    render() {
+        return (
+            <div className="input__wrapper">
+                <input
+                    type="text"
+                    placeholder="Username"
+                    onChange={this.userHandle}
+                    value={this.props.value}
+                >
+                </input>
+                {this.props.value.match(/^[a-zA-z0-9._-]{4,32}$/)
+                    ? <div className="tooltip tooltip__green"><i className="fa fa-check"></i></div>
+                    : <div className="tooltip">!<UserTooltip /></div>}
+            </div>
+        );
+    }
+}
+
+function UserTooltip(props) {
+    return (
+        <div className="tooltip__text">
+            <span>Username must be between 4 and 32 characters<br />
+            Username may contain _, -, .
+            </span>
+        </div>
+    )
+}
+
+class Email extends React.Component {
+    constructor(props) {
+        super(props);
+        this.emailChange = this.emailChange.bind(this);
+    }
+
+    emailChange(e) {
+        this.props.emailValuePass(e);
+    }
+
+    render() {
+        return (
+            <div className="input__wrapper">
+                <input
+                    placeholder="Email"
+                    onChange={this.emailChange}
+                    type="email"
+                    value={this.props.value}
+                ></input>
+            </div>
+        );
+    }
+}
+
+class Password extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            passShow: false,
+        };
+        this.passChange = this.passChange.bind(this);
+        this.togglePassword = this.togglePassword.bind(this);
+    }
+
+    togglePassword(event) {
+        this.setState({ passShow: this.state.passShow ? false : true });
+        event.target.classList.toggle("fa-eye-slash");
+    }
+
+    passChange(e) {
+        this.props.passwordValuePass(e);
+    }
+
+    render() {
+        let tooltipToggle = this.props.passCheck.checkNumber && this.props.passCheck.checkLower && this.props.passCheck.checkUpper && this.props.passCheck.length > 7;
+        return (
+            <div className="input__wrapper">
+                <input
+                    placeholder="Password"
+                    onChange={this.passChange}
+                    type={this.state.passShow ? "text" : "password"}
+                    value={this.props.value}
+                ></input><i className="far fa-eye" onClick={this.togglePassword}></i>
+                {tooltipToggle
+                    ? <div className="tooltip tooltip__green"><i className="fa fa-check"></i></div>
+                    : <div className="tooltip">!<PasswordTooltip /></div>}
+            </div>
+        )
+    }
+}
+
+function PasswordTooltip(props) {
+    return (
+        <div className="tooltip__text">
+            <span>Password Must:<br />
+            * be longer than 7 characters<br />
+            * contain at least one number<br />
+            * contain at least one upper and lower case letters</span>
+        </div>
+    )
+}
+
 function App() {
     return (
         <div>
-            Registration with Email
             <Form />
         </div>
     );
